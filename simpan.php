686ee2890727b3c858d3e7f87ac962604725d0ae
Dadang Iswanto <dadangiswan@gmail.com>
2013-07-10 14:24:23 +0700

    tambah function remove kata jorok

--- simpan.php
+++ simpan.php
@@ -1,35 +1,52 @@
 <?php
 
 include 'koneksi.php';
-
+include 'block_xx.php';
 if ($_GET[link] == "saveRegister") {
-    if ($_POST[nama_lengkap] != null && $_POST[username] != null && $_POST[password] != null && $_POST[confirm] != null && $_POST[email] != null && $_POST[no_hp] != null) {
-        if ($_POST[password] == $_POST[confirm]) {
-            $query = mysql_query("INSERT INTO `tugas_akhir_12101020`.`register` (`nama_lengkap`, `username`, `password`, `email`, `no_hp`, `akses`) VALUES ('$_POST[nama_lengkap]', '$_POST[username]', '$_POST[password]', '$_POST[email]', '$_POST[no_hp]', '$_POST[akses]')");
-            if ($query) {
-                echo"
-            <script type=\"text/javascript\">
-                alert(\"Data berhasil disimpan.\")
-                location='index.php?link=sukses_register';
-            </script>
-        ";
-            } else {
-                echo"
-            <script type=\"text/javascript\">
-                alert(\"Data gagal disimpan..!!\")
-                location='index.php?link=sukses_register';
-            </script>
-        ";
-            }
-        } else {
+    $nama_lengkap = $_POST[nama_lengkap];
+    $username = $_POST[username];
+    $password = $_POST[password];
+    $confirm = $_POST[confirm];
+    $email = $_POST[email];
+    $no_hp = $_POST[no_hp];
+    if ($nama_lengkap != null && $username != null && $password != null && $confirm != null && $email != null && $no_hp != null) {
+        if (cek_pesan($username)) {
             echo"
             <script type=\"text/javascript\">
-                alert(\"Sandi anda tidak falid..!!\")
-                location='index.php?link=register';
-            </script>
-        ";
-        }
-    } else {
+            alert(\"kata kotor telah kami blokir dan tidak dapat dijadikan username.\")
+            location='index.php?link=register';
+            </script>";
+            }
+ 
+            else{
+                if ($_POST[password] == $_POST[confirm]) {
+                    $query = mysql_query("INSERT INTO `tugas_akhir_12101020`.`register` (`nama_lengkap`, `username`, `password`, `email`, `no_hp`, `akses`) VALUES ('$_POST[nama_lengkap]', '$_POST[username]', '$_POST[password]', '$_POST[email]', '$_POST[no_hp]', '$_POST[akses]')");
+                    if ($query) {
+                        echo"
+                    <script type=\"text/javascript\">
+                        alert(\"Data berhasil disimpan.\")
+                        location='index.php?link=sukses_register';
+                    </script>
+                ";
+                    } else {
+                        echo"
+                    <script type=\"text/javascript\">
+                        alert(\"Data gagal disimpan..!!\")
+                        location='index.php?link=sukses_register';
+                    </script>
+                ";
+                    }
+                } else {
+                    echo"
+                    <script type=\"text/javascript\">
+                        alert(\"Sandi anda tidak falid..!!\")
+                        location='index.php?link=register';
+                    </script>
+                ";
+                }
+            } 
+    
+    }else {
         echo"
             <script type=\"text/javascript\">
                 alert(\"Data Tidak Lengkap.\")
