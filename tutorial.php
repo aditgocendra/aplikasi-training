14a641a047e1c886e01d2e450d6ad7a5529beff3
dadangiswan <dadangiswan@gmail.com>
2013-09-17 09:25:12 +0700

    edit petunjuk

--- tutorial.php
+++ tutorial.php
@@ -1,10 +1,20 @@
 <div style="padding-top: 20px">
-<div class="alert alert-info">
-  <p><strong>Petunjuk Pembayaran Training :</strong></p>
-  <p>1. Setelah anda melakukan pendaftaran training, anda akan mendapat balasan melalui email dari hrd@mediainfo.co.id yang berisi nomor rekening perusahaan untuk melakukan pembayaran via transfer dan format untuk balas email kembali setelah melakukan pembayaran via transfer.</p>
-  <p>2. Setelah anda melakukan transfer, anda wajib mengirim email balasan ke hrd@mediainfo.co.id dengan format yang sudah di kirim oleh kami melalui email dan bukti transfer pembayaran anda.</p>
-  <p>3. Tunggu 1x24 jam maka status pembayaran pada website akan berubah menjadi SUDAH_LUNAS dan anda akan mendapat konfirmasi email lagi bahwa pembayaran anda sudah lunas.</p>
-  <p>4. Status SUDAH_LUNAS bisa di lihat di halaman pengunjung ==> menu Peserta Training).</p>
+    <div class="alert alert-info">
+        <p><strong>Petunjuk Pendaftaran Training :</strong></p>
+        <p>1. Buka website kami di www.mediainfo.co.id.</p>
+        <p>2. Pilih menu register untuk melakukan pendaftaran akun pada website kami.</p>
+        <p>3. Setelah menu register muncul, selanjutnya isi form pendaftaran sesuai identitas lengkap anda, lalu klik simpan.</p>
+        <p>4. Setelah anda register, klik menu login untuk memasukkan username dan password anda.</p>
+        <p>5. Setelah anda login, klik menu daftar training untuk melakukan pendaftaran training.</p>
+        <p>6. Setelah menu daftar training muncul, lalu isi paket training yang anda inginkan, lalu simpan.</p>
+        <p>7. Setelah anda simpan, lalu check pada menu peserta training, jika berhasil, maka data daftar training anda masuk pada tabel peserta training dengan status BELUM_LUNAS.</p>
+        <p>8. Untuk melakukan pembayaran, caranya bisa lihat pada petunjuk pembayaran training.</p>
+    </div>
+    <div class="alert alert-info">
+        <p><strong>Petunjuk Pembayaran Training :</strong></p>
+        <p>1. Setelah anda melakukan pendaftaran training, anda akan mendapat balasan melalui email dari hrd@mediainfo.co.id yang berisi nomor rekening perusahaan untuk melakukan pembayaran via transfer dan format untuk balas email kembali setelah melakukan pembayaran via transfer.</p>
+        <p>2. Setelah anda melakukan transfer, anda wajib mengirim email balasan ke hrd@mediainfo.co.id dengan format yang sudah di kirim oleh kami melalui email dan bukti transfer pembayaran anda.</p>
+        <p>3. Tunggu 1x24 jam maka status pembayaran pada website akan berubah menjadi SUDAH_LUNAS dan anda akan mendapat konfirmasi email lagi bahwa pembayaran anda sudah lunas.</p>
+        <p>4. Status SUDAH_LUNAS bisa di lihat di halaman pengunjung ==> menu Peserta Training).</p>
+    </div>
 </div>
-</div>
-    
